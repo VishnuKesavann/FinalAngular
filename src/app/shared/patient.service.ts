e6741669a003b95257820ba16df574be6946c079
JezinNK123 <jezinnelson@gmail.com>
2024-01-19 14:03:14 +0530

    added the changes in paitent

--- src/app/shared/patient.service.ts
+++ src/app/shared/patient.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Patient } from './patient';
 import {HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import {environment} from 'src/environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
 //Declared a patient class
-  patient:Patient[];
+  patients:Patient[];
   constructor(private httpClient:HttpClient) { }
+BindListPatients(){
+  this.httpClient.get(environment.apiUrl+"api/RPatient").toPromise().then(
+    response=>{
+      this.patients=response as Patient[];
+      console.log(this.patients);
+    }
+  ).catch(error=>{console.error('Error: ',error)});
+}
+
 }
