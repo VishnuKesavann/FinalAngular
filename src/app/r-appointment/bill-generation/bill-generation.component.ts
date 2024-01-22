f590f802c550a0ff3f11417ca1eabae8e08639f8
JezinNK123 <jezinnelson@gmail.com>
2024-01-22 19:12:03 +0530

    Generated the bill for the appointment 1/22/20247:11

--- src/app/r-appointment/bill-generation/bill-generation.component.ts
+++ src/app/r-appointment/bill-generation/bill-generation.component.ts
@@ -1,15 +1,40 @@
+import { DatePipe } from '@angular/common';
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import {Billviewmodel} from 'src/app/shared/billviewmodel'
+import {BillviewmodelService} from 'src/app/shared/billviewmodel.service'
 @Component({
   selector: 'app-bill-generation',
   templateUrl: './bill-generation.component.html',
   styleUrls: ['./bill-generation.component.scss']
 })
 export class BillGenerationComponent implements OnInit {
-
-  constructor() { }
+  BillId:number;
+  bill_r:Billviewmodel=new Billviewmodel();
+  constructor(private route:ActivatedRoute,private BillingService:BillviewmodelService) { }
 
   ngOnInit(): void {
+    this.BillId=this.route.snapshot.params['BillId'];
+    console.log('populating Bill Details');
+    console.log('BillID:',this.BillId);
+    this.BillingService.getBill(this.BillId).subscribe(
+      data=>
+      {
+       console.log(data);
+       this.bill_r=data;
+
+        var datePipe=new DatePipe('en-UK');
+        let formatedyear:any=datePipe.transform(data.AppointmentDate,'yyyy-MM-dd');
+        data.AppointmentDate=formatedyear;
+
+        this.BillingService.bill_r=Object.assign({},data);
+        console.log(FormData);
+      },error=>{
+        console.log(error);
+      }
+    )
   }
 
+  
 }
--- src/app/shared/billviewmodel.service.ts
+++ src/app/shared/billviewmodel.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Billviewmodel } from './billviewmodel';
 @Injectable({
   providedIn: 'root'
 })
 export class BillviewmodelService {
-
-  constructor() { }
+  bill_r:Billviewmodel=new Billviewmodel();
+  constructor(private httpClient:HttpClient) { }
+  //Populating Patient Records
+getBill(BillId:number):Observable<any>{
+  return this.httpClient.get(environment.apiUrl+'api/RAppoinment/GetBillDetails/'+BillId);
+}
 }
